1a9c8d4e3970af31dbfe1f1047b7d658e20fe2ab
Caopower <2415720923@qq.com>
2018-12-25 19:29:07 +0800

    LLLL

--- app/src/main/java/demo/com/LoginActivity.java
+++ app/src/main/java/demo/com/LoginActivity.java
@@ -4,7 +4,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class LoginActivity extends Fragment {//余松彦
+public class LoginActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_login, null);
         return view;
