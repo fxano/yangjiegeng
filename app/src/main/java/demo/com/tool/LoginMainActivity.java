1db87013153578470e05aaac9929e484851245ea
Caopower <2415720923@qq.com>
2018-12-26 20:32:43 +0800

    ScrollView

--- app/src/main/java/demo/com/tool/LoginMainActivity.java
+++ app/src/main/java/demo/com/tool/LoginMainActivity.java
@@ -1,18 +1,25 @@
 package demo.com.tool;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.WindowManager;
-
 import demo.com.R;
-
+import demo.com.login_fragment.Login_LandEmail;
+import demo.com.login_fragment.Login_LandEmailTwo;
 public class LoginMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.owninfo_activity);
+        setContentView(R.layout.loginmain_activity);
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        Login_LandEmail landing = new Login_LandEmail();
+        transaction.add(R.id.loginFrag, landing);
+        transaction.commit();
         getAndroiodScreenProperty();
     }
     public void getAndroiodScreenProperty(){
