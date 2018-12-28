4a9df1c5cc09f6bd5be6df9ab73cf3d4aa5e172e
y18890388615 <yl1396575113@sina.com>
2018-12-28 10:27:37 +0800

    Yu

--- app/src/main/java/demo/com/OwnRelease.java
+++ app/src/main/java/demo/com/OwnRelease.java
@@ -6,14 +6,23 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import demo.com.adapter.Adapter_MyRelease;
+import demo.com.adapter.Entity_MyRelease;
+
 public class OwnRelease extends Activity {
+    private List<Entity_MyRelease> list;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ownrelease);
         RelativeLayout imgback =findViewById(R.id.imgback);
+        list=new ArrayList<>();
         imgback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +31,19 @@ public class OwnRelease extends Activity {
 //                finish();
             }
         });
+        initData();
+        ListView lv = findViewById(R.id.lv);
+        lv.setAdapter(new Adapter_MyRelease(list,this));
+    }
+
+    private void initData() {
+        for(int i=0;i<10;i++){
+            Entity_MyRelease em = new Entity_MyRelease();
+            em.setHead(R.mipmap.pic);
+            em.setName("小余"+i);
+            em.setTime("06:1"+i);
+            em.setTitle("妆出你想要的样子！"+i);
+            list.add(em);
+        }
     }
 }
